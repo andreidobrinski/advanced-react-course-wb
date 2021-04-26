83702e6077758bc80644213dc097626460266c4d
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-04-26 17:52:18 -0400

    add User schema

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,5 +1,6 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
+import { User } from './schemas/User';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -24,6 +25,7 @@ export default config({
   },
   lists: createSchema({
     // TODO: schema items go here
+    User,
   }),
   ui: {
     // TODO: change this for roles
