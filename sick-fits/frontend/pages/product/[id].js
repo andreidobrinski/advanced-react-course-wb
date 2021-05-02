d2af21297a6050f9544c78a46400b247ecdc1d4b
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-05-02 17:39:13 -0400

    prefer useRouter to get pageProps

--- sick-fits/frontend/pages/product/[id].js
+++ sick-fits/frontend/pages/product/[id].js
@@ -1,5 +1,7 @@
+import { useRouter } from 'next/router';
 import SingleProduct from '../../components/SingleProduct';
 
-export default function SingleProductPage({ query }) {
+export default function SingleProductPage() {
+  const { query } = useRouter();
   return <SingleProduct id={query.id} />;
 }
