76f0b64466f41a403e1477a27b0ca336ac14c20a
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-05-02 17:47:34 -0400

    start on update product

---
+++ sick-fits/frontend/components/UpdateProduct.js
@@ -0,0 +1,21 @@
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+
+  return <p>update</p>;
+}
