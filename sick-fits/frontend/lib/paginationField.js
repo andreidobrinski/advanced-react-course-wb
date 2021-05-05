9586a18858f1ebb2a3a948778ca3d70fa3b40e35
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-05-05 16:59:01 -0400

    custom type policy with apollo

---
+++ sick-fits/frontend/lib/paginationField.js
@@ -0,0 +1,40 @@
+import { PAGINATION_QUERY } from '../components/Pagination';
+
+export default function paginationField() {
+  return {
+    keyArgs: false,
+    read(existing = [], { args, cache }) {
+      const { skip, first } = args;
+
+      const data = cache.readQuery({ query: PAGINATION_QUERY });
+      const count = data?._allProductsMeta?.count;
+      const page = skip / first + 1;
+      const pages = Math.ceil(count / first);
+
+      const items = existing.slice(skip, skip + first).filter((x) => x);
+
+      if (items.length !== first) {
+        // fetch from network
+        return false;
+      }
+
+      // return items from cache
+      if (items.length) {
+        return items;
+      }
+
+      // fallback to network request
+      return false;
+    },
+    merge(existing, incoming, { args }) {
+      const { skip, first } = args;
+      const merged = existing ? existing.slice(0) : [];
+
+      for (let i = skip; i < skip + incoming.length; ++i) {
+        merged[i] = incoming[i - skip];
+      }
+
+      return merged;
+    },
+  };
+}
