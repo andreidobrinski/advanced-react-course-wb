5826b3e1cf77aba4df5ef07a65e0fc9c7355de6a
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-05-31 08:14:38 -0400

    add permissions check

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,6 +1,20 @@
+import { permissionsList } from "./schemas/fields";
 import { ListAccessArgs } from "./types";
 
 // access control is a yes or no value depending on the user's session
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+// permissions check
+const generatedPermissions = Object.fromEntries(permissionsList.map(permission => [
+  permission,
+  function({ session }: ListAccessArgs) {
+    return !!session?.data.role?.[permission];
+  }
+]));
+
+export const permissions = {
+  ...generatedPermissions,
+
+};
